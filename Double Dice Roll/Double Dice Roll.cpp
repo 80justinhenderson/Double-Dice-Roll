c20edf294d747d85d3584504053fdd0d483cae19
NMMI <NMMI@5CG1266BJC-CDT>
2022-04-14 13:49:03 -0600

    Add project files.

---
+++ Double Dice Roll/Double Dice Roll.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+void functionDice30()
+{
+	void functionDice0();
+	{
+		srand(time(0));
+
+		int dice1 = rand() % 6 + 1;
+		int dice2 = rand() % 6 + 1;
+		cout << dice2 << endl;
+	}
+	void functionDice1();
+	{
+		srand(time(0));
+
+		int dice2 = rand() % 6 + 1;
+		cout << dice2 << endl;
+	}
+
+}
+
+
+	int main()
+{
+	functionDice30();
+}
